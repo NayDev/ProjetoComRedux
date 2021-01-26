a20ad50e0f8ca5ffab8ca49cf763090459c3f763
Nayara <nayara.lopes2007@gmail.com>
2021-01-25 23:16:59 -0300

    Finalizando reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,19 +15,31 @@ export default function App() {
 function addCompra(event){
 
   event.preventDefault();
-  const objCompra = {
-    nome: inputCompras
-  }
-  dispatch( {type: "ADICIONAR", value: objCompra});
+  // const objCompra = {
+  //   nome: inputCompras
+  // }
+  
+  dispatch( {type: "ADICIONAR", nome: inputCompras});
+  setInputCompras("");
 }
+ function removeCompras(event) {
+  event.preventDefault();
+   //alert(event.target.id);
 
+   dispatch( {type: "REMOVE", id: event.target.id});
+
+ }
 function altTitulo(event) {
   setInputTitulo(event.target.value);
   dispatch( {type: "ALTERAR", value: event.target.value});
+  
 }
 
 return (
-  <div className="d-flex justify-content-between p-3"> 
+  <>
+    <h2 className="text-center my-5 bg-warning text-secondary">Avaliação de Redux</h2>
+  <div className="d-flex justify-content-between p-3 bg-light rounded shadow-lg"> 
+ 
     <section className="bg-primary container w-50 m-5">
       <h1>Vamos Fazer as Compras?</h1>
         <form onSubmit={addCompra}>
@@ -52,9 +64,9 @@ return (
           return (
             <tbody>
               <tr>
-                <th scope="row">1</th>
+                <th scope="row">{compra.id}</th>
                 <td>{compra.nome}</td>
-                <td><button className="btn btn-danger my-2">Excluir</button></td>
+                <td><button id={compra.id} className="btn btn-danger my-2" onClick={removeCompras}>Excluir</button></td>
               </tr>
             </tbody>
           )
@@ -62,13 +74,24 @@ return (
       </table>
     </section>
 
-    <section className="container bg-success my-5 w-50 m-5">
+    <section className="container bg-info my-5 w-50 m-5">
       <form>
         <h1>{stateTitulo}</h1>
-        <input placeholder="Digite o título" value={inputTitulo} onChange={altTitulo}/>
+        <input  className="form-control my-4"  placeholder="Digite um título para Trocar" value={inputTitulo} onChange={altTitulo}/>
       </form>
     </section>
+  
   </div>
+  <div class="text-center p-3" >
+  
+    © 2021 Nayara Suelen 2021 - Recode Pro
+  </div>
+  <div> 
+    
+
+    <p></p>
+  </div>
+  </>
 );
 }
 
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,18 +1,26 @@
 import { combineReducers } from 'redux';
 
+let contador = 0;
+
 const initialState = {
   compras: []
 }
 
 function comprasReducer(state = initialState, action) {
   if(action.type === "ADICIONAR") {
-    return { compras: [...state.compras, {...action.value }]}
-  } else {
-      return state;
+    return { compras: [...state.compras, {id: contador++, nome: action.nome}]}
+  } else if(action.type === "REMOVE") {
+  
+    return { compras: (state.compras).filter(item => item.id != action.id)}
+
+  } else{
+    return state;
   }
 }
 
-function tituloReducer(state = {titulo: "Vamos trocar o Titulo?"}, action) {
+
+
+function tituloReducer(state = {titulo: "Título"}, action) {
   if(action.type === "ALTERAR"){
     return {titulo: action.value};
   } else{
@@ -20,6 +28,6 @@ function tituloReducer(state = {titulo: "Vamos trocar o Titulo?"}, action) {
   }
 }
 
-const reducers = combineReducers( { comprasReducer, tituloReducer } );
+const reducers = combineReducers( { comprasReducer ,tituloReducer } );
 
 export default reducers;
